d89e2d06abf5e3a05015b28b4d8ece902f068fb7
Ankita Shinde <65124563+Ankita2040@users.noreply.github.com>
2020-08-04 11:40:41 +0530

    Update game.py

--- game.py
+++ game.py
@@ -11,8 +11,13 @@ WHITE = (255,255,255)
 global score
 score = 0
 
-WIDTH = 800
-HEIGHT = 500
+BALL_IMG = pygame.image.load(os.path.join("images", "ball.png"))
+BACKGROUND = pygame.image.load(os.path.join("images", "background.png"))
+PLATFORM_IMG = pygame.image.load(os.path.join("images", "platform.png"))
+WALL_BLOCK = pygame.image.load(os.path.join("images", "wall.png"))
+
+WIDTH = 780
+HEIGHT = 480
 
 PLATFORM_WIDTH = 70
 PLATFORM_HEIGHT = 20
@@ -25,10 +30,17 @@ BALL_RADIUS = 15
 PLATFORM_POS = [y for y in range(BORDER_WIDTH // 2, WIDTH - PLATFORM_WIDTH - BORDER_WIDTH // 2)]
 
 
-def drawBackground(win):
-	win.fill(RED)
-	pygame.draw.rect(win, LIGHT_RED, (0, 0, WIDTH, HEIGHT + BORDER_WIDTH), BORDER_WIDTH)
+def drawWall(win):
+	# win.blit(BACKGROUND, (0, 0)) #win.fill(RED)
+	# pygame.draw.rect(win, LIGHT_RED, (0, 0, WIDTH, HEIGHT + BORDER_WIDTH), BORDER_WIDTH)
+	for i in range(HEIGHT// (BORDER_WIDTH//2)):
+		win.blit(WALL_BLOCK, (0, i * (BORDER_WIDTH//2)))
+		win.blit(WALL_BLOCK, (WIDTH - BORDER_WIDTH//2, i * (BORDER_WIDTH//2)))
+
+	for i in range(1, WIDTH//(BORDER_WIDTH//2) - 1):
+		win.blit(WALL_BLOCK, (i * (BORDER_WIDTH//2), 0))
 	pygame.display.update()
+
 
 
 class Ball:
@@ -43,9 +55,11 @@ class Ball:
 		
 
 	def draw(self, win):
-		pygame.draw.circle(win, RED, (self.prY, self.prX), BALL_RADIUS)
-		pygame.draw.circle(win, WHITE, (self.y, self.x), BALL_RADIUS)
-		pygame.draw.rect(win, LIGHT_RED, (0, 0, WIDTH, HEIGHT + BORDER_WIDTH), BORDER_WIDTH)
+		# pygame.draw.circle(win, RED, (self.prY, self.prX), BALL_RADIUS)
+		# pygame.draw.circle(win, WHITE, (self.y, self.x), BALL_RADIUS)
+		win.blit(BALL_IMG, (self.y, self.x))
+		drawWall(win)
+		# pygame.draw.rect(win, LIGHT_RED, (0, 0, WIDTH, HEIGHT + BORDER_WIDTH), BORDER_WIDTH)
 		pygame.display.update()
 
 	def drawEnd(self, win):
@@ -83,9 +97,11 @@ class PLatform:
 		self.vel = 5
 
 	def draw(self, win):
-		pygame.draw.rect(win, RED, (self.prY, self.prX, PLATFORM_WIDTH, PLATFORM_HEIGHT), 0)
-		pygame.draw.rect(win, WHITE, (self.y, self.x, PLATFORM_WIDTH, PLATFORM_HEIGHT), 0)
-		pygame.draw.rect(win, LIGHT_RED, (0, 0, WIDTH, HEIGHT + BORDER_WIDTH), BORDER_WIDTH)
+		# pygame.draw.rect(win, RED, (self.prY, self.prX, PLATFORM_WIDTH, PLATFORM_HEIGHT), 0)
+		# pygame.draw.rect(win, WHITE, (self.y, self.x, PLATFORM_WIDTH, PLATFORM_HEIGHT), 0)
+		win.blit(PLATFORM_IMG, (self.y, self.x))
+		drawWall(win)
+		# pygame.draw.rect(win, LIGHT_RED, (0, 0, WIDTH, HEIGHT + BORDER_WIDTH), BORDER_WIDTH)
 		pygame.display.update()
 
 	def drawEnd(self, win):
@@ -127,7 +143,8 @@ def eval_genomes(genomes, config):
 	for platform in platforms:
 		balls.append(Ball(platform))
 
-	drawBackground(win)
+	win.blit(BACKGROUND, (0, 0))
+	drawWall(win)
 
 	run = True
 	while run:
@@ -145,8 +162,8 @@ def eval_genomes(genomes, config):
 			finalOP = output.index(max(output)) - 1
 
 			platforms[x].move(finalOP)
-			platforms[x].draw(win)
-			ball.draw(win)
+			# platforms[x].draw(win)
+			# ball.draw(win)
 
 			
 
@@ -164,12 +181,20 @@ def eval_genomes(genomes, config):
 					ge[x].fitness += 10
 					ball.score += 1
 				
-				if ge[x].fitness >= 10000:
-					print("SCORE -> {}".format(balls[x].score))
-					run = False
-					break
+				# if ge[x].fitness >= 10000:
+				# 	print("SCORE -> {}".format(balls[x].score))
+				# 	run = False
+				# 	break
 
+		win.blit(BACKGROUND, (0,0))
+		drawWall(win)
 
+		for ball in balls:
+			ball.draw(win)
+
+		for platform in platforms:
+			platform.draw(win)
+		pygame.display.update()
 			
 
 		if len(balls) == 0:
@@ -198,27 +223,3 @@ if __name__ == "__main__":
 	local_dir = os.path.dirname(__file__)
 	config_path = os.path.join(local_dir, "config-FeedForward.txt")
 	run(config_path)
-
-
-# FINAL NETWORK -> 
-
-# Nodes:
-
-# 	0 DefaultNodeGene(key=0, bias=0.4681362068901073, response=1.0, activation=tanh, aggregation=sum)
-# 	1 DefaultNodeGene(key=1, bias=-0.7847066751326026, response=1.0, activation=tanh, aggregation=sum)
-# 	2 DefaultNodeGene(key=2, bias=-0.9954854092123031, response=1.0, activation=tanh, aggregation=sum)
-
-# Connections:
-
-# 	DefaultConnectionGene(key=(-4, 0), weight=1.6285013685748344, enabled=False)
-# 	DefaultConnectionGene(key=(-4, 2), weight=-0.7101061666647931, enabled=True)
-# 	DefaultConnectionGene(key=(-3, 0), weight=2.067433038877517, enabled=True)
-# 	DefaultConnectionGene(key=(-3, 1), weight=0.734692619956036, enabled=False)
-# 	DefaultConnectionGene(key=(-3, 2), weight=0.9306699673288863, enabled=True)
-# 	DefaultConnectionGene(key=(-2, 0), weight=-0.23500221405740196, enabled=True)
-# 	DefaultConnectionGene(key=(-2, 1), weight=1.5154625453785813, enabled=True)
-# 	DefaultConnectionGene(key=(-2, 2), weight=-2.674277591063213, enabled=True)
-# 	DefaultConnectionGene(key=(-1, 0), weight=-0.624714514876567, enabled=True)
-# 	DefaultConnectionGene(key=(-1, 1), weight=0.8375018129099269, enabled=True)
-# 	DefaultConnectionGene(key=(-1, 2), weight=-0.060159958203100605, enabled=True)
-# 	DefaultConnectionGene(key=(0, 0), weight=0.35188613188996326, enabled=True)
